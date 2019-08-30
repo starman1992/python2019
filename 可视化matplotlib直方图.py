4cb5f5e567369acb129dff3b6b7c66e9cc221c69
starman1992 <54134962+starman1992@users.noreply.github.com>
2019-08-30 11:45:39 +0800

    Add files via upload

--- 可视化matplotlib直方图.py
+++ 可视化matplotlib直方图.py
@@ -1,6 +1,13 @@
 ##折线图、散点图plot
 import matplotlib.pylab as pyl
 import numpy as npy
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+import numpy as np
+
+
+my_font = font_manager.FontProperties(fname="C:\Windows\Fonts\STSONG.TTF")
+
 pyl.subplot(3,1,1)#定位图片显示位置(行，列，当前区域位置)
 x=[1,2,3,4,8]
 y=[5,7,2,1,5]
@@ -17,35 +24,16 @@ pyl.plot(x,y,'c-.')
 
 
 '''
-c-cyan-青色
-r-red
-m-magente-品红
-g-green
-b-blue
-y-yellow
-k-black
-w-white
+c-cyan-青色  r-red  m-magente-品红  g-green  b-blue  y-yellow  k-black  w-white
 '''
 
 ##线条样式
 '''
-- 直线
---  虚线
--.  就是-.
-:   细小虚线
-'''
+- 直线  --  虚线  -.  就是-.  :   细小虚线  '''
 
 #点的样式
 '''
-s--方形
-h--六角形
-H--六角形
-*--星形
-+--加号
-x--x型
-d--菱形
-D--菱形
-p--五角形
+s--方形  h--六角形  H--六角形  *--星形+--加号  x--x型  d--菱形  D--菱形  p--五角形
 '''
 
 pyl.subplot(3,2,3)
@@ -58,18 +46,46 @@ pyl.xlabel('ages')
 pyl.ylabel('temp')
 pyl.xlim(0,20)#横轴范围
 pyl.ylim(0,40)#纵轴范围
+plt.xticks(range(2,26,2))#设置横轴的刻度位置
 
 #随机数的生成
-import numpy as npy
-
 pyl.subplot(3,2,4)#定位图片显示位置(行，列，当前区域位置)
-data1 = npy.random.normal(10.0,1.0,1000)#(均数,sigma，个数)
+data1 = np.random.normal(10.0,1.0,1000)#(均数,sigma，个数)
 pyl.hist(data1)
+
 
 #分布直方图hist(x轴参数范围，y轴参数个数)
 pyl.subplot(3,1,3)
-data2 = npy.random.randint(1,25,1000)#(最小值，最大值，个数)
-sty = npy.arange(2,19,4)#设置直方图宽即x轴(最小值，最大值，步长)
+data2 = np.random.randint(1,25,1000)#(最小值，最大值，个数)
+sty = np.arange(2,19,4)#设置直方图宽即x轴(最小值，最大值，步长)
 pyl.hist(data2,sty)#绘制直方图，sty为直方图格式如上
 
+#温度分布10-12点
+x3=range(0,120)
+y3=[np.random.randint(20,35) for i in range(120)]
+plt.figure(figsize=(10,6),dpi=80)#设置格式大小，清晰度
+plt.plot(x3,y3)
+
+_x = list(x3)[::10]#修改步长
+_xtick_labels = ["10点{}分".format(i) for i in range(60)]
+_xtick_labels += ["11点{}分".format(i) for i in range(60)]
+plt.xticks(x3[::10],_xtick_labels[::10],rotation=45,fontproperties=my_font)#修改横轴名字,把横轴坐标竖过来
+##plt.savefig('./t1.png')保存图片
+
+
 pyl.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
